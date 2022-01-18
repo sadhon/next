d9129f19668f91ea415c590fdcd3da420b93e838
sadhon <odesksadhon@gmail.com>
2022-01-18 21:40:45 +0600

    trying to fix the bug

--- pages/404.js
+++ pages/404.js
@@ -1,24 +1,24 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
-const error = () => {
-  const router = useRouter();
-  useEffect(() => {
-    setTimeout(() => {
-      router.push('/');
-    }, 3000);
-  }, []);
+const Error = () => {
+	const router = useRouter();
+	useEffect(() => {
+		setTimeout(() => {
+			router.push("/");
+		}, 3000);
+	}, []);
 
-  return (
-    <>
-      <h3>404 error page</h3>
-      <p>
-        This is not exist. go to the
-        <br /> <Link href='/'> Homepage</Link>
-      </p>
-    </>
-  );
+	return (
+		<>
+			<h3>404 Error page</h3>
+			<p>
+				This is not exist. go to the
+				<br /> <Link href="/"> Homepage</Link>
+			</p>
+		</>
+	);
 };
 
-export default error;
+export default Error;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
-import Navbar from '../components/Navbar';
-import Image from 'next/image';
+import Navbar from "../components/Navbar";
+import Image from "next/image";
 
 const index = () => {
-  return (
-    <>
-      <Navbar />
-      <h1>This is homepage</h1>
-      <Image src='/img/nature.jpg' width={500} height={300} />
-      <br />
-      <Image
-        src='http://softtech-it.com/wp-content/uploads/2021/07/laravel-react-course.jpg'
-        width={500}
-        height={300}
-      />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<h1>This is homepage</h1>
+			<Image
+				alt="Just Image for practice"
+				src="/img/nature.jpg"
+				width={500}
+				height={300}
+			/>
+			<br />
+			<Image
+				alt="Just Image for practice"
+				src="http://softtech-it.com/wp-content/uploads/2021/07/laravel-react-course.jpg"
+				width={500}
+				height={300}
+			/>
+		</>
+	);
 };
 
 export default index;
